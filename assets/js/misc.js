7b6f52d96ee1f712c625b09121367954fe7363ea
alexcote30 <alexandre.cote30@gmail.com>
2022-04-30 14:58:25 -0400

    tests

---
+++ assets/js/misc.js
@@ -0,0 +1,14 @@
+$(function loadFooterHeader() {
+    $( "#footer" ).load( "footer.html" );
+    $( "#header" ).load( "header.html" );
+    var selectedClass = "";
+    $("p").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#portfolio").fadeTo(50, 0.1);
+        $("#portfolio div").not("." + selectedClass).fadeOut();
+    setTimeout(function() {
+      $("." + selectedClass).fadeIn();
+      $("#portfolio").fadeTo(50, 1);
+    }, 500);
+    });
+});
